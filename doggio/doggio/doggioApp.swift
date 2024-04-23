0d588b7e79e190ff001c65f5eae2add77f668688
Jeffrey Sheets <84060666+Jeffrey-Sheets@users.noreply.github.com>
2024-04-22 20:17:49 -0400

    Add files via upload

---
+++ doggio/doggio/doggioApp.swift
@@ -0,0 +1,17 @@
+//
+//  doggioApp.swift
+//  doggio
+//
+//  Created by Jeffrey Sheets on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct doggioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
